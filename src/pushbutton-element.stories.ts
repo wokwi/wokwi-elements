adfc79cb785ab796ff246069f07c827342018af4
Uri Shaked <uri@urishaked.com>
2020-01-10 23:50:14 +0200

    test(pushbutton): add "4 buttons" story

--- src/pushbutton-element.stories.ts
+++ src/pushbutton-element.stories.ts
@@ -25,4 +25,30 @@ storiesOf('pushbutton', module)
           @button-release=${action('button-release')}
         ></wokwi-pushbutton>
       `
+  )
+  .add(
+    'Four buttons',
+    () =>
+      html`
+        <wokwi-pushbutton
+          color="red"
+          @button-press=${action('red button-press')}
+          @button-release=${action('red button-release')}
+        ></wokwi-pushbutton>
+        <wokwi-pushbutton
+          color="green"
+          @button-press=${action('green button-press')}
+          @button-release=${action('green button-release')}
+        ></wokwi-pushbutton>
+        <wokwi-pushbutton
+          color="blue"
+          @button-press=${action('blue button-press')}
+          @button-release=${action('blue button-release')}
+        ></wokwi-pushbutton>
+        <wokwi-pushbutton
+          color="white"
+          @button-press=${action('white button-press')}
+          @button-release=${action('white button-release')}
+        ></wokwi-pushbutton>
+      `
   );
