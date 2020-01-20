99ad53748ff29edaec977c01cf8094db91a9d362
Uri Shaked <uri@urishaked.com>
2020-01-20 11:50:04 +0200

    fix(storybook): full page reload on update
    HMR is currently broken with lit-element: https://www.npmjs.com/package/@storybook/web-components#setup-page-reload-via-hmr

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,12 @@
 import { configure } from '@storybook/polymer';
 
 // automatically import all files ending in *.stories.ts
-configure(require.context('../src', true, /\.stories\.ts$/), module);
+const req = require.context('../src', true, /\.stories\.ts$/);
+configure(req, module);
+if (module.hot) {
+  module.hot.accept(req.id, () => {
+    const currentLocationHref = window.location.href;
+    window.history.pushState(null, null, currentLocationHref);
+    window.location.reload();
+  });
+}
