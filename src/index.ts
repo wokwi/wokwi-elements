53c9621bb8461acbc79ee9e9a5efbeb1ec8ef0cf
Uri Shaked <uri@urishaked.com>
2020-03-26 00:01:11 +0200

    fix(uno): missing export for ArduinoUnoElement
    it was not exported by index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
-export { LEDElement } from './led-element';
-export { PushbuttonElement } from './pushbutton-element';
-export { ResistorElement } from './resistor-element';
 export { SevenSegmentElement } from './7segment-element';
+export { ArduinoUnoElement } from './arduino-uno-element';
 export { LCD1602Element } from './lcd1602-element';
 export { fontA00 } from './lcd1602-font-a00';
 export { fontA02 } from './lcd1602-font-a02';
+export { LEDElement } from './led-element';
 export { NeoPixelElement } from './neopixel-element';
+export { PushbuttonElement } from './pushbutton-element';
+export { ResistorElement } from './resistor-element';
