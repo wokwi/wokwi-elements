bb0c6f4fa09c72b80089fe3c79ed6335d4ae9038
Uri Shaked <uri@urishaked.com>
2020-03-30 13:51:02 +0300

    build(storybook): set up analytics addon
    so that we get an idea how many people are looking at our storybook :-)

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,8 @@
 module.exports = {
-  addons: ['@storybook/addon-docs'],
+  addons: [
+    '@storybook/addon-actions/register',
+    '@storybook/addon-docs',
+    '@storybook/addon-google-analytics/register',
+    '@storybook/addon-knobs/register',
+  ],
 };
---
+++ .storybook/manager.js
@@ -0,0 +1,1 @@
+window.STORYBOOK_GA_ID = 'UA-150413053-2';
