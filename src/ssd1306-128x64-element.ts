358d367752ecb53d12952c13854390dfcef2ffb3
lironh <lironenk@gmail.com>
2020-04-09 07:22:36 +0300

    feat(ssd1306): font fallback and style tweaks

--- src/ssd1306-128x64-element.ts
+++ src/ssd1306-128x64-element.ts
@@ -3,9 +3,16 @@ import { customElement, html, LitElement, property, SVGTemplateResult } from 'li
 
 @customElement('wokwi-ssd1306-128x64-element')
 export class Ssd1306128x64Element extends LitElement {
-  width = 150;
-  height = 116;
-  @property() imageData: ImageData = new ImageData(128, 64);
+  readonly width = 150;
+  readonly height = 116;
+  private screenWidth = 128;
+  private screenHeight = 64;
+  @property() imageData: ImageData;
+
+  constructor() {
+    super();
+    this.imageData = new ImageData(this.screenWidth, this.screenHeight);
+  }
 
   putGivenImageData() {
     const canvas: HTMLCanvasElement | null | undefined = this.shadowRoot?.querySelector('canvas');
@@ -19,12 +26,12 @@ export class Ssd1306128x64Element extends LitElement {
   }
 
   render(): SVGTemplateResult {
-    const { width, height } = this;
+    const { width, height, screenWidth, screenHeight } = this;
     return html`<svg width="${width}" height="${height}" xmlns="http://www.w3.org/2000/svg">
       <g>
         <rect stroke="#BE9B72" fill="#025CAF" x=".5" y=".5" width="148" height="114" rx="13" />
 
-        <g transform="translate(6 6)" fill="#59340A" stroke="#BE9B72">
+        <g transform="translate(6 6)" fill="#59340A" stroke="#BE9B72" stroke-width="0.6px">
           <circle cx="130" cy="6" r="5.5" />
           <circle cx="6" cy="6" r="5.5" />
           <circle cx="130" cy="96" r="5.5" />
@@ -33,10 +40,10 @@ export class Ssd1306128x64Element extends LitElement {
 
         <g transform="translate(8 26)">
           <!-- 128 x 64 screen -->
-          <rect fill="#1A1A1A" width="128" height="64" />
+          <rect fill="#1A1A1A" width="${screenWidth}" height="${screenHeight}" />
           <!-- image holder -->
-          <foreignObject width="128" height="64">
-            <canvas width="128" height="64"></canvas>
+          <foreignObject width="${screenWidth}" height="${screenHeight}">
+            <canvas width="${screenWidth}" height="${screenHeight}"></canvas>
           </foreignObject>
         </g>
 
@@ -46,7 +53,7 @@ export class Ssd1306128x64Element extends LitElement {
           text-anchor="middle"
           font-size="5"
           font-weight="300"
-          font-family="MarkerFelt-Wide, Marker Felt"
+          font-family="MarkerFelt-Wide, Marker Felt, monospace"
         >
           <g transform="translate(37 3)">
             <text x="0" y="5">Data</text>
@@ -68,24 +75,24 @@ export class Ssd1306128x64Element extends LitElement {
             stroke="#FFF"
           />
 
-          <text transform="translate(24 3)" x="0" y="102">v2.1</text>
+          <text transform="translate(25 3)" x="0" y="102">v2.1</text>
 
-          <text transform="translate(40.5 3)">
+          <text transform="translate(40 3)">
             <tspan font-size="5" x="102" y="75">V5</tspan>
             <tspan font-size="3" x="102" y="81" font-weight="300">READY</tspan>
           </text>
         </g>
 
         <!-- PINS -->
-        <g transform="translate(33 9)" fill="#9D9D9A">
-          <circle cx="70.5" cy="3.5" r="3.5" />
-          <circle cx="60.5" cy="3.5" r="3.5" />
-          <circle cx="50.5" cy="3.5" r="3.5" />
-          <circle cx="41.5" cy="3.5" r="3.5" />
-          <circle cx="31.5" cy="3.5" r="3.5" />
-          <circle cx="21.5" cy="3.5" r="3.5" />
-          <circle cx="12.5" cy="3.5" r="3.5" />
-          <circle cx="3.5" cy="3.5" r="3.5" />
+        <g transform="translate(33 9)" fill="#9D9D9A" stroke-width="0.4">
+          <circle stroke="#262626" cx="70.5" cy="3.5" r="3.5" />
+          <circle stroke="#007ADB" cx="60.5" cy="3.5" r="3.5" />
+          <circle stroke="#9D5B96" cx="50.5" cy="3.5" r="3.5" />
+          <circle stroke="#009E9B" cx="41.5" cy="3.5" r="3.5" />
+          <circle stroke="#E8D977" cx="31.5" cy="3.5" r="3.5" />
+          <circle stroke="#C08540" cx="21.5" cy="3.5" r="3.5" />
+          <circle stroke="#B4AEAB" cx="12.5" cy="3.5" r="3.5" />
+          <circle stroke="#E7DBDB" cx="3.5" cy="3.5" r="3.5" />
         </g>
       </g>
     </svg> `;
