27851eca9e762cb3e32e19da0794d42b0ab976a7
gfeun <gfeun@protonmail.com>
2020-04-25 15:57:36 +0200

    revert to 2 svg, remove transforms

--- src/buzzer-element.ts
+++ src/buzzer-element.ts
@@ -25,6 +25,7 @@ export class BuzzerElement extends LitElement {
 
       .music-note {
         position: relative;
+        left: 40px;
         animation-duration: 1.5s;
         animation-name: animate-note;
         animation-iteration-count: infinite;
@@ -32,7 +33,7 @@ export class BuzzerElement extends LitElement {
         transform: scale(1.5);
         fill: blue;
         offset-path: path(
-          'm0 -12c-0.9-0.92-1.8-1.8-2.4-2.8-0.56-0.92-0.78-1.8-0.58-2.8 0.2-0.92 0.82-1.8 1.6-2.8 0.81-0.92 1.8-1.8 2.6-2.8 0.81-0.92 1.4-1.8 1.6-2.8 0.2-0.92-0.02-1.8-0.58-2.8-0.56-0.92-1.5-1.8-2.4-2.8'
+          'm0 0c-0.9-0.92-1.8-1.8-2.4-2.8-0.56-0.92-0.78-1.8-0.58-2.8 0.2-0.92 0.82-1.8 1.6-2.8 0.81-0.92 1.8-1.8 2.6-2.8 0.81-0.92 1.4-1.8 1.6-2.8 0.2-0.92-0.02-1.8-0.58-2.8-0.56-0.92-1.5-1.8-2.4-2.8'
         );
         offset-rotate: 0deg;
       }
@@ -63,24 +64,26 @@ export class BuzzerElement extends LitElement {
     return html`
       <div class="buzzer-container">
         <svg
+          class="music-note"
+          style="visibility: ${buzzerOn ? '' : 'hidden'}"
+          xmlns="http://www.w3.org/2000/svg"
+          width="8"
+          height="8"
+          viewBox="0 0 8 8"
+        >
+          <path
+            d="M8 0c-5 0-6 1-6 1v4.09c-.15-.05-.33-.09-.5-.09-.83 0-1.5.67-1.5 1.5s.67 1.5 1.5 1.5 1.5-.67 1.5-1.5v-3.97c.73-.23 1.99-.44 4-.5v2.06c-.15-.05-.33-.09-.5-.09-.83 0-1.5.67-1.5 1.5s.67 1.5 1.5 1.5 1.5-.67 1.5-1.5v-5.5z"
+          />
+        </svg>
+        <svg
           width="17mm"
-          height="42.862mm"
+          height="17mm"
           version="1.1"
-          viewBox="0 0 17 42.862"
+          viewBox="0 0 17 17"
           xmlns="http://www.w3.org/2000/svg"
         >
-          <g transform="matrix(.84999 0 0 .84999 8.5115e-5 -209.59)">
-            <circle cx="10" cy="287" r="9.5001" stroke="#000" />
-            <g transform="matrix(.98546 0 0 .98546 .14539 4.1728)">
-              <circle cx="10" cy="287" r="1.3673" fill="#ccc" stroke="#000" stroke-width=".25" />
-            </g>
-          </g>
-          <path
-            class="music-note"
-            style="visibility: ${buzzerOn ? '' : 'hidden'}"
-            d="m9.4927 22.263c-1.9844 0-2.3812 0.39688-2.3812 0.39688v1.6232c-0.05954-0.01984-0.13096-0.03572-0.19844-0.03572-0.32941 0-0.59531 0.26591-0.59531 0.59531 0 0.32941 0.26591 0.59531 0.59531 0.59531 0.32941 0 0.59531-0.26591 0.59531-0.59531v-1.5756c0.28972-0.09128 0.78978-0.17462 1.5875-0.19844v0.81756c-0.05953-0.01984-0.13096-0.03572-0.19844-0.03572-0.32941 0-0.59531 0.26591-0.59531 0.59531 0 0.32941 0.26591 0.59531 0.59531 0.59531 0.32941 0 0.59531-0.26591 0.59531-0.59531z"
-            stroke-width=".39688"
-          />
+          <circle cx="8.5" cy="8.5" r="8.075" stroke="#000" stroke-width=".85" />
+          <circle cx="8.5" cy="8.5" r="1.3744" fill="#ccc" stroke="#000" stroke-width=".25" />
         </svg>
       </div>
     `;
