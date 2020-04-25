9f6f6f9f19198067732704ab2d62a5151039a315
gfeun <gfeun@protonmail.com>
2020-04-25 16:30:56 +0200

    refine design: add wires + inner circles

--- src/buzzer-element.ts
+++ src/buzzer-element.ts
@@ -75,15 +75,45 @@ export class BuzzerElement extends LitElement {
             d="M8 0c-5 0-6 1-6 1v4.09c-.15-.05-.33-.09-.5-.09-.83 0-1.5.67-1.5 1.5s.67 1.5 1.5 1.5 1.5-.67 1.5-1.5v-3.97c.73-.23 1.99-.44 4-.5v2.06c-.15-.05-.33-.09-.5-.09-.83 0-1.5.67-1.5 1.5s.67 1.5 1.5 1.5 1.5-.67 1.5-1.5v-5.5z"
           />
         </svg>
+
         <svg
           width="17mm"
-          height="17mm"
+          height="19.975mm"
           version="1.1"
-          viewBox="0 0 17 17"
+          viewBox="0 0 17 19.975"
           xmlns="http://www.w3.org/2000/svg"
         >
-          <circle cx="8.5" cy="8.5" r="8.075" stroke="#000" stroke-width=".85" />
-          <circle cx="8.5" cy="8.5" r="1.3744" fill="#ccc" stroke="#000" stroke-width=".25" />
+          <path d="m7.925 16.975v3" fill="none" stroke="#000" stroke-width=".5" />
+          <path d="m8.925 16.975v3" fill="#f00" stroke="#f00" stroke-width=".5" />
+          <g stroke="#000">
+            <g>
+              <ellipse
+                cx="8.5"
+                cy="8.5"
+                rx="8.1469"
+                ry="8.1469"
+                fill="#1a1a1a"
+                stroke-width=".70623"
+              />
+              <circle
+                cx="8.425"
+                cy="8.575"
+                r="6.2309"
+                fill="none"
+                stroke-width=".3"
+                style="paint-order:normal"
+              />
+              <circle
+                cx="8.425"
+                cy="8.575"
+                r="4.3137"
+                fill="none"
+                stroke-width=".3"
+                style="paint-order:normal"
+              />
+            </g>
+            <circle cx="8.425" cy="8.575" r="1.3744" fill="#ccc" stroke-width=".25129" />
+          </g>
         </svg>
       </div>
     `;
