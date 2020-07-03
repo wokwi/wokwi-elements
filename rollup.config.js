c7dccc86c94db94bfeb14d601602bd82917ca0be
Uri Shaked <uri@urishaked.com>
2020-07-04 00:46:24 +0300

    feat: generate minified dist bundle #30
    using rollup and terser to produce the bundle. close #30

---
+++ rollup.config.js
@@ -0,0 +1,19 @@
+// rollup.config.js
+import resolve from 'rollup-plugin-node-resolve';
+import commonJS from 'rollup-plugin-commonjs';
+
+export default {
+  input: 'dist/esm/index.js',
+  output: {
+    file: 'dist/wokwi-elements.bundle.js',
+    name: 'elements',
+    format: 'iife',
+  },
+  context: 'window',
+  plugins: [
+    resolve(),
+    commonJS({
+      include: 'node_modules/**',
+    }),
+  ],
+};
