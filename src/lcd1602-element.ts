957ffb465b02d3f38349aea1e1e4070751b84a09
Uri Shaked <uri@urishaked.com>
2020-08-16 21:23:35 +0300

    feat(lcd1602): add pinInfo

--- src/lcd1602-element.ts
+++ src/lcd1602-element.ts
@@ -1,5 +1,6 @@
 import { customElement, html, LitElement, property, css, svg } from 'lit-element';
 import { fontA00 } from './lcd1602-font-a00';
+import { ElementPin, i2c } from './pin';
 
 const ROWS = 2;
 const COLS = 16;
@@ -46,6 +47,39 @@ export class LCD1602Element extends LitElement {
         }
       }
     `;
+  }
+
+  get pinInfo(): ElementPin[] {
+    switch (this.pins) {
+      case 'i2c':
+        return [
+          { name: 'GND', x: 4, y: 32, number: 1, signals: [{ type: 'power', signal: 'GND' }] },
+          { name: 'VCC', x: 4, y: 41.5, number: 2, signals: [{ type: 'power', signal: 'VCC' }] },
+          { name: 'SDA', x: 4, y: 51, number: 3, signals: [i2c('SDA')] },
+          { name: 'SCL', x: 4, y: 60.5, number: 4, signals: [i2c('SCL')] },
+        ];
+
+      case 'full':
+      default:
+        return [
+          { name: 'VSS', x: 32, y: 131, number: 1, signals: [{ type: 'power', signal: 'GND' }] },
+          { name: 'VDD', x: 41.5, y: 131, number: 2, signals: [{ type: 'power', signal: 'VCC' }] },
+          { name: 'V0', x: 51.5, y: 131, number: 3, signals: [] },
+          { name: 'RS', x: 60.5, y: 131, number: 4, signals: [] },
+          { name: 'RW', x: 70.5, y: 131, number: 5, signals: [] },
+          { name: 'E', x: 80, y: 131, number: 6, signals: [] },
+          { name: 'D0', x: 89.5, y: 131, number: 7, signals: [] },
+          { name: 'D1', x: 99.5, y: 131, number: 8, signals: [] },
+          { name: 'D2', x: 109, y: 131, number: 9, signals: [] },
+          { name: 'D3', x: 118.5, y: 131, number: 10, signals: [] },
+          { name: 'D4', x: 128, y: 131, number: 11, signals: [] },
+          { name: 'D5', x: 137.5, y: 131, number: 12, signals: [] },
+          { name: 'D6', x: 147, y: 131, number: 13, signals: [] },
+          { name: 'D7', x: 156.5, y: 131, number: 14, signals: [] },
+          { name: 'A', x: 166.5, y: 131, number: 15, signals: [] },
+          { name: 'K', x: 176, y: 131, number: 16, signals: [] },
+        ];
+    }
   }
 
   path(characters: Uint8Array | number[]) {
