d9d72176ec4b5d78d7c8748be252843ee08fd11c
Søren Ranneries <spinkelben@gmail.com>
2021-04-20 19:35:51 +0200

    Improved visual design. Cleaned up svg

--- src/slide-potentiometer-element.ts
+++ src/slide-potentiometer-element.ts
@@ -17,76 +17,69 @@ export class SlidePotentiometerElement extends LitElement {
         xmlns:xlink="http://www.w3.org/1999/xlink"
       >
         <defs>
-          <linearGradient id="linearGradient1085">
+          <linearGradient
+            id="tipGradient"
+            x1="36.482"
+            x2="50.447"
+            y1="91.25"
+            y2="91.25"
+            gradientTransform="matrix(.8593 0 0 1.1151 -14.849 -92.256)"
+            gradientUnits="userSpaceOnUse"
+          >
             <stop stop-color="#1a1a1a" offset="0" />
-            <stop stop-color="#6f6f6f" offset="1" />
+            <stop stop-color="#595959" offset=".4" />
+            <stop stop-color="#595959" offset=".6" />
+            <stop stop-color="#1a1a1a" offset="1" />
           </linearGradient>
-          <linearGradient
-            id="linearGradient1003"
-            x1="47.774"
-            x2="60.67"
-            y1="48.576"
-            y2="48.643"
-            gradientTransform="matrix(.81818 0 0 1 -106.69 -110.55)"
+          <radialGradient
+            id="bodyGradient"
+            cx="62.59"
+            cy="65.437"
+            r="22.5"
+            gradientTransform="matrix(1.9295 3.7154e-8 0 .49697 -98.268 -23.02)"
             gradientUnits="userSpaceOnUse"
-            xlink:href="#linearGradient1085"
-          />
-          <linearGradient
-            id="linearGradient1089"
-            x1="47.774"
-            x2="60.439"
-            y1="48.576"
-            y2="48.549"
-            gradientTransform="matrix(.81818 0 0 1 18.486 20.324)"
-            gradientUnits="userSpaceOnUse"
-            xlink:href="#linearGradient1085"
-          />
+          >
+            <stop stop-color="#d2d2d2" offset="0" />
+            <stop stop-color="#2a2a2a" offset="1" />
+          </radialGradient>
         </defs>
-        <g transform="translate(-40.09 -55.937)">
+        <g>
+          <!-- Body -->
           <rect
-            x="40.09"
-            y="60.937"
+            x="0"
+            y="5"
             width="45"
             height="9"
             rx=".2"
             ry=".2"
-            fill="#767676"
+            fill="url(#bodyGradient)"
             fill-rule="evenodd"
           />
-          <rect x="43.352" y="63.366" width="38.5" height="4.1427" rx=".1" ry=".1" fill="#6e3333" />
-          <g transform="rotate(13.024 41.817 65.256)" stroke="#000">
-            <circle cx="41.817" cy="65.256" r=".92486" fill="#858585" stroke-width=".05" />
-            <path d="m41.368 66.064 0.8975-1.6155" fill="none" stroke-width=".151" />
+          <rect x="3.25" y="8" width="38.5" height="3" rx=".1" ry=".1" fill="#3f1e1e" />
+          <!-- Screw Left -->
+          <g transform="translate(1.625 9.5) rotate(45)">
+            <circle cx="0" cy="0" r="1" fill="#858585" stroke="#000" stroke-width=".05" />
+            <path d="m0 1 0-2" fill="none" stroke="#000" stroke-width=".151" />
           </g>
-          <g transform="rotate(16.386 62.16 209.21)" stroke="#000">
-            <circle cx="41.817" cy="65.256" r=".92486" fill="#858585" stroke-width=".05" />
-            <path d="m41.368 66.064 0.8975-1.6155" fill="none" stroke-width=".151" />
+          <!-- Button -->
+          <g transform="">
+            <rect x="19.75" y="8.6" width="5.5" height="1.8" />
+            <rect
+              x="16.5"
+              y="0"
+              width="12"
+              height="19"
+              fill="url(#tipGradient)"
+              stroke-width="2.6518"
+              rx=".1"
+              ry=".1"
+            />
+            <rect x="22.2" y="0" width=".6" height="19" fill="#efefef" />
           </g>
-          <g>
-            <rect x="59.84" y="64.537" width="5.5" height="1.8" />
-            <rect
-              x="56.59"
-              y="55.937"
-              width="4.5"
-              height="19"
-              rx=".3"
-              ry=".3"
-              fill="url(#linearGradient1089)"
-              style="mix-blend-mode:normal"
-            />
-            <rect
-              transform="scale(-1)"
-              x="-68.59"
-              y="-74.937"
-              width="4.5"
-              height="19"
-              rx=".3"
-              ry=".3"
-              fill="url(#linearGradient1003)"
-              style="mix-blend-mode:normal"
-            />
-            <rect x="60.828" y="55.937" width="3.5" height="19" fill="#171717" />
-            <rect x="62.29" y="55.937" width=".6" height="19" fill="#efefef" />
+          <!-- Screw Right -->
+          <g transform="translate(43.375 9.5) rotate(45)">
+            <circle cx="0" cy="0" r="1" fill="#858585" stroke="#000" stroke-width=".05" />
+            <path d="m0 1 0-2" fill="none" stroke="#000" stroke-width=".151" />
           </g>
         </g>
       </svg>
