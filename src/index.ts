e8f5b85e37ee778a4be56d0672ea43f7b63a76ac
AriellaE <35613240+AriellaE@users.noreply.github.com>
2021-06-06 13:16:40 +0300

    feat: add small sound sensor

--- src/index.ts
+++ src/index.ts
@@ -36,3 +36,4 @@ export { FlameSensorElement } from './flame-sensor-element';
 export { GasSensorElement } from './gas-sensor-element';
 export { FranzininhoElement } from './franzininho-element';
 export { NanoRP2040ConnectElement } from './nano-rp2040-connect-element';
+export { SmallSoundSensorElement } from './small-sound-sensor-element';
