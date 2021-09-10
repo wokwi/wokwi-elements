0fd764e750b70518aecfe13e9b588f84978153b5
Uri Shaked <uri@urishaked.com>
2021-09-10 18:11:09 +0300

    fix: broken import #108

--- src/react-types.ts
+++ src/react-types.ts
@@ -42,7 +42,7 @@ import { ESP32DevkitV1Element } from './esp32-devkit-v1-element';
 import { KY040Element } from './ky-040-element';
 import { PhotoresistorSensorElement } from './photoresistor-sensor-element';
 import { RGBLedElement } from './rgb-led-element';
-import { ILI9341Element } from './ILI9341-element';
+import { ILI9341Element } from './ili9341-element';
 
 type WokwiElement<T> = Partial<T> & React.ClassAttributes<T>;
 
