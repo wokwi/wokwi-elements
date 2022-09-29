caefdefee157ab9b03ce78ea4d485d7606316877
Ariella Eliassaf <ariella@codemagic.co.il>
2022-09-29 21:14:08 +0300

    feat(stepper-motor): add size attribute #138

--- src/stepper-motor-element.stories.ts
+++ src/stepper-motor-element.stories.ts
@@ -6,6 +6,7 @@ export default {
   component: 'wokwi-stepper-motor',
   argTypes: {
     angle: { control: { type: 'range', min: 0, max: 360 } },
+    size: { control: { type: 'select', options: [8, 11, 14, 17, 23, 34] } },
     arrow: { control: { type: 'color' } },
   },
   args: {
@@ -13,25 +14,39 @@ export default {
     arrow: '',
     units: '',
     value: '',
+    size: 23,
   },
 };
 
-const Template = ({ angle, arrow, units, value }) =>
+const Template = ({ angle, arrow, units, value, size }) =>
   html`<wokwi-stepper-motor
     .angle=${angle}
     .arrow=${arrow}
     .units=${units}
     .value=${value}
+    .size=${size}
   ></wokwi-stepper-motor>`;
 
 export const Default = Template.bind({});
 Default.args = {};
 
 export const Rotated90 = Template.bind({});
-Rotated90.args = { angle: 90, units: 'degrees', value: '90' };
+Rotated90.args = { angle: 90, units: 'degrees', value: '90', size: 14 };
 
 export const Steps = Template.bind({});
-Steps.args = { angle: 180, value: '52,500', units: 'steps' };
+Steps.args = { angle: 180, value: '52,500', units: 'steps', size: 14 };
+
+export const Degrees = Template.bind({});
+Degrees.args = { angle: 180, value: '180', units: 'degrees', size: 14 };
 
 export const PurpleArrow = Template.bind({});
-PurpleArrow.args = { angle: 70, arrow: '#4a36ba' };
+PurpleArrow.args = { angle: 350, arrow: '#4a36ba', size: 14 };
+
+export const Nema17 = Template.bind({});
+Nema17.args = { angle: 70, arrow: '#4a36ba', size: 17 };
+
+export const Nema23 = Template.bind({});
+Nema23.args = { angle: 70, arrow: '#4a36ba', value: '1234', units: 'steps', size: 23 };
+
+export const Nema34 = Template.bind({});
+Nema34.args = { angle: 70, arrow: '#4a36ba', value: '180', units: 'degrees', size: 34 };
--- src/stepper-motor-element.ts
+++ src/stepper-motor-element.ts
@@ -1,177 +1,315 @@
 import { html, LitElement } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
 import { ElementPin } from '.';
+import { mmToPix } from './utils/units';
+
+/**
+ * NemaSpec describes a NEMA Stepper specification (for the purpose of visualisation)
+ *
+ */
+export interface NEMASpec {
+  id: number; // Nema common number representing the size shorthand (Nema11 Nema 17 etc)
+  frameSize: number; // the frame size in mm. Since Nema Steppers are square, only one side needed
+  holeRadius: number; // Fastening hole size
+  shaftRadius: number; // Motor shaft radius
+  cornerRadius: number; // Frame corner radius
+  cornerOffset: number; // Offset from corner to center of hole
+  bodyRadius: number; // the round motor body size
+  textSize: number; // Text size showing units etc
+  /**  Y position of value text */
+  valueYPosition: number; 
+  /**  Y position of units text */
+  unitsYPosition: number;  
+}
+
+const defaultSize = 23;
 
 @customElement('wokwi-stepper-motor')
 export class StepperMotorElement extends LitElement {
-  @property() angle = 0;
+  @property({ type: Number }) angle = 0;
   @property() arrow = '';
   @property() value = '';
   @property() units = '';
+  @property() size: 8 | 11 | 14 | 17 | 23 | 34 = defaultSize;
 
-  readonly pinInfo: ElementPin[] = [
-    { name: 'A+', y: 237, x: 108, number: 1, signals: [] },
-    { name: 'A-', y: 237, x: 98, number: 2, signals: [] },
-    { name: 'B+', y: 237, x: 117, number: 3, signals: [] },
-    { name: 'B-', y: 237, x: 127, number: 4, signals: [] },
-  ];
+  get pinInfo(): ElementPin[] {
+    const spec = this.nemaSpecMap[this.size] ?? this.nemaSpecMap[defaultSize];
+
+    // these offsets match the transform in renderFace
+    const xOff = (spec.frameSize / 2 - 3.75) * mmToPix + 1;
+    const yOff = (spec.frameSize + 5) * mmToPix;
+
+    const pi = [
+      { name: 'A-', y: yOff, x: xOff, number: 1, signals: [] },
+      { name: 'A+', y: yOff, x: xOff + 2.54 * mmToPix, number: 2, signals: [] },
+      { name: 'B+', y: yOff, x: xOff + 5.08 * mmToPix, number: 3, signals: [] },
+      { name: 'B-', y: yOff, x: xOff + 7.62 * mmToPix, number: 4, signals: [] },
+    ];
+
+    return pi;
+  }
+
+  readonly nemaSpecMap: { [key: string]: NEMASpec } = {
+    '8': {
+      id: 8,
+      frameSize: 20.4,
+      holeRadius: 0.5,
+      shaftRadius: 3.5,
+      cornerRadius: 2.5,
+      cornerOffset: 2.5,
+      bodyRadius: 7.7,
+      textSize: 10,
+      valueYPosition: 16.5,
+      unitsYPosition: 19.7,
+    },
+    '11': {
+      id: 11,
+      frameSize: 28.2,
+      holeRadius: 1.25,
+      shaftRadius: 5,
+      cornerRadius: 2.5,
+      cornerOffset: 2.5,
+      bodyRadius: 11,
+      textSize: 12,
+      valueYPosition: 21.5,
+      unitsYPosition: 24,
+    },
+    '14': {
+      id: 14,
+      frameSize: 35.2,
+      holeRadius: 1.5,
+      shaftRadius: 5,
+      cornerRadius: 4.5,
+      cornerOffset: 4.5,
+      bodyRadius: 11,
+      textSize: 14,
+      valueYPosition: 26,
+      unitsYPosition: 32,
+    },
+    '17': {
+      id: 17,
+      frameSize: 42.3,
+      holeRadius: 1.5,
+      shaftRadius: 5,
+      cornerRadius: 5,
+      cornerOffset: 5.5,
+      bodyRadius: 14,
+      textSize: 16,
+      valueYPosition: 30.5,
+      unitsYPosition: 33.7,
+    },
+    '23': {
+      id: 23,
+      frameSize: 57.3,
+      holeRadius: 2.5,
+      shaftRadius: 6.35,
+      cornerRadius: 5,
+      cornerOffset: 5.5,
+      bodyRadius: 19.5,
+      textSize: 24,
+      valueYPosition: 41,
+      unitsYPosition: 46,
+    },
+    '34': {
+      id: 34,
+      frameSize: 86,
+      holeRadius: 3.25,
+      shaftRadius: 14,
+      cornerRadius: 3.25,
+      cornerOffset: 8.4,
+      bodyRadius: 36.5,
+      textSize: 32,
+      valueYPosition: 68,
+      unitsYPosition: 75,
+    },
+  };
 
   render() {
-    const { arrow } = this;
+    const spec = this.nemaSpecMap[this.size] ?? this.nemaSpecMap[defaultSize];
 
-    return html`
-      <svg
-        width="57mm"
-        height="63.349mm"
-        version="1.1"
-        viewBox="0 0 57 63.349"
-        xmlns="http://www.w3.org/2000/svg"
-        xmlns:xlink="http://www.w3.org/1999/xlink"
-      >
-        <defs>
-          <linearGradient
-            id="motor-body"
-            x1="17.567"
-            x2="197.27"
-            y1="171.8"
-            y2="171.8"
-            gradientTransform="matrix(.31539 0 0 .31539 9.0021 56.33)"
-            gradientUnits="userSpaceOnUse"
-          >
-            <stop stop-color="#666" offset="0" />
-            <stop stop-color="#fff" offset="1" />
-          </linearGradient>
-          <linearGradient
-            id="linearGradient5454"
-            x1="339.07"
-            x2="478.3"
-            y1="660.79"
-            y2="660.79"
-            gradientTransform="matrix(.083446 0 0 .083446 9.845 56.471)"
-            gradientUnits="userSpaceOnUse"
-          >
-            <stop stop-color="#9d9d9d" offset="0" />
-            <stop stop-color="#9d9d9d" stop-opacity="0" offset="1" />
-          </linearGradient>
-          <linearGradient
-            id="linearGradient29501"
-            x1="33.418"
-            x2="53.905"
-            y1="114.39"
-            y2="113.37"
-            gradientUnits="userSpaceOnUse"
-          >
-            <stop stop-color="#9d9d9d" offset="0" />
-            <stop stop-color="#fdfafa" offset=".29501" />
-            <stop offset="1" />
-          </linearGradient>
-        </defs>
+    const cornerRadius = spec.cornerRadius;
+    const holeRadius = spec.holeRadius;
+    const shaftRadius = spec.shaftRadius;
+    const frameSize = spec.frameSize;
+    const cornerOffset = spec.cornerOffset;
+    const bodyRadius = spec.bodyRadius;
 
-        <!-- Body -->
-        <g transform="translate(-14.38 -82.486)">
+    const halfShaft = shaftRadius / 2;
+    const halfFrame = frameSize / 2;
+
+    const innerHoleRadius = holeRadius * 0.9;
+    const outerHoleRadius = holeRadius * 1.1;
+
+    // shaft radius offset, needed for transform
+    const rOff = Math.sqrt(0.75 * Math.pow(shaftRadius, 2));
+
+    return html`<svg
+      width="${frameSize + 1}mm"
+      height="${frameSize + 5}mm"
+      version="1.1"
+      viewBox="0 0 ${(1 + frameSize) * mmToPix} ${(5 + frameSize) * mmToPix}"
+      xmlns="http://www.w3.org/2000/svg"
+      xmlns:xlink="http://www.w3.org/1999/xlink"
+    >
+      <defs>
+        <linearGradient
+          id="frame-gradient"
+          x1="-${frameSize * 0.2}"
+          x2="${frameSize * 2}"
+          y1="${frameSize}"
+          y2="${frameSize}"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stop-color="#666" offset="0" />
+          <stop stop-color="#fff" offset="1" />
+        </linearGradient>
+        <linearGradient
+          id="shaft-gradient"
+          x1="0"
+          x2="0"
+          y1="-5"
+          y2="5"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stop-color="#9d9d9d" offset="0" />
+          <stop stop-color="#9d9d9d" stop-opacity="0" offset="1" />
+        </linearGradient>
+        <linearGradient
+          id="body-gradient"
+          x1="${frameSize * 0.1}"
+          x2="${frameSize * 0.7}"
+          y1="${frameSize}"
+          y2="${frameSize}"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stop-color="#9d9d9d" offset="0" />
+          <stop stop-color="#fdfafa" offset=".29501" />
+          <stop offset="1" stop-color="#2a2a2a" />
+        </linearGradient>
+      </defs>
+      <!-- Body -->
+      <g transform="translate(1,1)">
+        <g transform="scale(${mmToPix})">
+          <!-- Pins -->
+          <path
+            id="pin"
+            transform="translate(${halfFrame - 3.75} ${frameSize})"
+            fill="#9f9f9f"
+            d="m 0 0 c .5 0 .5 0 .5 .5 v 4.55 c -.5 .5 -.5 .5 -1 0 v -4.5 c 0 -.5 0 -.5 .5 -.5"
+          />
+          <use xlink:href="#pin" x="2.54" />
+          <use xlink:href="#pin" x="5.08" />
+          <use xlink:href="#pin" x="7.62" />
+
           <g stroke-linecap="round" stroke-linejoin="round">
             <rect
-              x="14.543"
-              y="82.648"
-              width="56.675"
-              height="55.731"
-              rx="4.7308"
-              ry="4.6584"
-              fill="url(#motor-body)"
+              width="${frameSize}"
+              height="${frameSize}"
+              rx="${cornerRadius}"
+              ry="${cornerRadius}"
+              fill="url(#frame-gradient)"
               stroke="#000"
               stroke-width=".3245"
             />
-            <circle cx="20.407" cy="88.675" r="1.7314" fill="#666" stroke-width=".47984" />
+            <circle cx="${cornerOffset}" cy="${cornerOffset}" r="${outerHoleRadius}" fill="#666" />
             <circle
-              cx="65.732"
-              cy="88.585"
-              r="1.7314"
-              fill="#666"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width=".47984"
+              cx="${cornerOffset}"
+              cy="${cornerOffset}"
+              r="${innerHoleRadius}"
+              fill="#e6e6e6"
             />
-            <ellipse cx="65.704" cy="88.613" rx="1.0232" ry=".99314" fill="#e6e6e6" />
             <circle
-              cx="66.278"
-              cy="133.65"
-              r="1.7314"
+              cx="${frameSize - cornerOffset}"
+              cy="${cornerOffset}"
+              r="${outerHoleRadius}"
               fill="#666"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width=".47984"
             />
-            <ellipse cx="66.25" cy="133.54" rx="1.0232" ry=".99314" fill="#e6e6e6" />
             <circle
-              cx="19.6"
-              cy="133.13"
-              r="1.7314"
-              fill="#666"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width=".47984"
+              cx="${frameSize - cornerOffset}"
+              cy="${cornerOffset}"
+              r="${innerHoleRadius}"
+              fill="#e6e6e6"
             />
-            <ellipse cx="19.572" cy="133.16" rx="1.0232" ry=".99314" fill="#ccc" />
-            <ellipse cx="20.378" cy="88.702" rx="1.0232" ry=".99314" fill="#ccc" />
+            <circle
+              cx="${cornerOffset}"
+              cy="${frameSize - cornerOffset}"
+              r="${outerHoleRadius}"
+              fill="#666"
+            />
+            <circle
+              cx="${cornerOffset}"
+              cy="${frameSize - cornerOffset}"
+              r="${innerHoleRadius}"
+              fill="#e6e6e6"
+            />
+            <circle
+              cx="${frameSize - cornerOffset}"
+              cy="${frameSize - cornerOffset}"
+              r="${outerHoleRadius}"
+              fill="#666"
+            />
+            <circle
+              cx="${frameSize - cornerOffset}"
+              cy="${frameSize - cornerOffset}"
+              r="${innerHoleRadius}"
+              fill="#e6e6e6"
+            />
+          </g>
 
-            <!-- Rotator -->
-            <circle
-              cx="43.816"
-              cy="111.05"
-              r="10.25"
-              fill="#868686"
-              fill-opacity=".89602"
-              opacity=".73"
-              stroke="url(#linearGradient29501)"
-              stroke-width=".41429"
+          <!-- motor body -->
+          <circle
+            cx="${halfFrame}"
+            cy="${halfFrame}"
+            r="${bodyRadius}"
+            fill="#868686"
+            fill-opacity=".89602"
+            opacity=".73"
+            stroke="url(#body-gradient)"
+            stroke-width="1.41429"
+          />
+          <!-- Rotator -->
+          <g>
+            <path
+              id="arrow-path"
+              transform="
+                rotate(${this.angle}, ${halfFrame},${halfFrame}) 
+                translate(${halfFrame} ${halfFrame})"
+              fill="${this.arrow || 'transparent'}"
+              d="m 0 0 l -${shaftRadius} 0 l ${shaftRadius} 
+                -${halfFrame - 3} l ${shaftRadius} ${halfFrame - 3} z"
             />
             <path
-              transform="rotate(${this.angle}, 43.82, 111.2)"
-              fill="${arrow || 'transparent'}"
-              d="M48.706051,111.66821H38.189949L43.448,83.34119Z"
-            />
-            <path
-              transform="rotate(${this.angle}, 43.82, 111.2)"
-              d="m40.056 106.86a5.3616 5.3616 0 0 0-1.9696 4.1497 5.3616 5.3616 0 0 0 5.3616 5.3616 5.3616 5.3616 0 0 0 5.3616-5.3616 5.3616 5.3616 0 0 0-1.9672-4.1497z"
+              id="shaft-path"
+              transform="
+                translate(${halfFrame}, ${halfFrame}) 
+                rotate(${this.angle}) 
+                translate(0, 0)"
+              d="m -${halfShaft} -${rOff} a ${shaftRadius} ${shaftRadius} 0 1 0 ${shaftRadius} 0 z"
               fill="#4d4d4d"
-              stroke="url(#linearGradient5454)"
+              stroke="url(#shaft-gradient)"
               stroke-width=".57968"
             />
           </g>
 
-          <g
-            transform="matrix(-.0031398 -.26456 .26456 -.0031398 32.829 149.58)"
-            clip-rule="evenodd"
-            fill-rule="evenodd"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-miterlimit="1.5"
-          >
-            <!-- Pins -->
-            <path
-              id="pin"
-              fill="#9f9f9f"
-              d="m15.259 56.3c-0.382-2.2e-5 -0.74801 0.15196-1.019 0.42194-0.27002 0.26998-0.42104 0.63698-0.42106 1.019-2.2e-5 0.382 0.15096 0.74801 0.42094 1.019 0.27098 0.27002 0.63698 0.42204 1.019 0.42206 5.242 2.96e-4 23.147 0.0013 26.132 0.0015 0.233 1.4e-5 0.42301-0.18998 0.42302-0.42398 3.1e-5 -0.545 8.4e-5 -1.489 1.15e-4 -2.035 1.3e-5 -0.233-0.18998-0.42301-0.42298-0.42302-2.985-1.68e-4 -20.89-0.0012-26.132-0.0015z"
-            />
-
-            <use xlink:href="#pin" y="-9.6" />
-            <use xlink:href="#pin" y="-19.2" />
-            <use xlink:href="#pin" y="-28.8" />
-
-            <!-- Text -->
-            <text
-              transform="rotate(90)"
-              font-family="arial"
-              font-size="14.667px"
-              text-align="center"
-              text-anchor="middle"
+          <!-- Text -->
+          <text font-family="arial" font-size="14.667px" text-align="center" text-anchor="middle">
+            <tspan
+              x="${halfFrame}"
+              y="${spec.valueYPosition}"
+              font-size="${spec.textSize / mmToPix}px"
             >
-              <tspan x="45" y="-58.62619" font-size="14px">${this.units}</tspan>
-              <tspan x="45" y="-75.066772" font-size="24px">${this.value}</tspan>
-            </text>
-          </g>
+              ${this.value}
+            </tspan>
+            <tspan
+              x="${halfFrame}"
+              y="${spec.unitsYPosition}"
+              font-size="${(0.7 * spec.textSize) / mmToPix}px"
+            >
+              ${this.units}
+            </tspan>
+          </text>
         </g>
-      </svg>
-    `;
+      </g>
+    </svg>`;
   }
 }
