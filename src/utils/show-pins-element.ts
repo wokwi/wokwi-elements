79eb6fe55fc2d1e3d8866a3a71213722c7a5dc7c
bobikenobi12 <borislav1207@gmail.com>
2024-02-26 19:21:45 +0200

    when pin is hovered change color

--- src/utils/show-pins-element.ts
+++ src/utils/show-pins-element.ts
@@ -27,9 +27,10 @@ export interface ElementWithPinInfo extends Element {
  */
 @customElement('wokwi-show-pins')
 export class ShowPinsElement extends LitElement {
-  @property() pinColor = 'red';
+  @property() pinColor = 'black';
   @query('#content') elementSlot!: HTMLSlotElement;
 
+  activePinIndex = -1;
   previousSlotChild?: ElementWithPinInfo;
 
   get slotChild() {
@@ -50,15 +51,36 @@ export class ShowPinsElement extends LitElement {
     this.requestUpdate();
   }
 
+  handleMouseOver(index: number) {
+    if (index !== this.activePinIndex) {
+      this.activePinIndex = index;
+      this.requestUpdate();
+    }
+  }
+  handleMouseOut() {
+    this.activePinIndex = -1; // Reset active pin to none
+    this.requestUpdate();
+  }
+
   render() {
     const pinInfo = this.slotChild?.pinInfo ?? [];
     const { pinColor } = this;
     return html`<div style="position: relative">
       <slot id="content" @slotchange=${() => this.handleSlotChange()}></slot>
-
-      <svg style="position: absolute; top: 0; left: 0" width="100%" height="100%" fill=${pinColor}>
+      <svg style="position: absolute; top: 0; left: 0;" width="100%" height="100%">
         ${pinInfo.map(
-          (pin) => svg`<circle cx=${pin.x} cy=${pin.y} r=2><title>${pin.name}</title></circle>`
+          (pin, idx) =>
+            svg`<rect 
+        x="${pin.x - 2.5}"
+        y="${pin.y - 2.5}"
+        fill=${this.activePinIndex === idx ? 'green' : pinColor}
+        width="5" 
+        height="5"
+        style="transition: fill 0.3s;"  
+        @mouseover=${this.handleMouseOver.bind(this, idx)}
+        @mouseout=${this.handleMouseOut}
+        @click=${() => console.log(pin)}
+      ><title>${pin.name}</title></rect>`
         )}
       </svg>
     </div>`;
